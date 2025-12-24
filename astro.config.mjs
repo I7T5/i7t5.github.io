437eb0b88da2e32b19c963f3041c39d05c26a070
Ina Tang <inatang75@outlook.com>
2025-12-24 00:15:16 +0000

    added remark-gfm, update astro and pnpm packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro'
+import remarkGfm from "remark-gfm";
 
 
 export default defineConfig({
   site: "https://i7t5.com",
-
   integrations: [
     UnoCSS({
       injectReset: true
     }),
   ],
+  markdown: {
+    remarkPlugins: [
+      remarkGfm, // footnotes, tables, strikethrough, autolinks, task lists
+    ],
+  },
 })
